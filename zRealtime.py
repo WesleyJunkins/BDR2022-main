d3aa8c5eadc154b8ad0225e116454a91e7b69abe
WesleyJunkins <Wjunkins13@yahoo.com>
2024-03-25 21:22:32 -0500

    cleanup

--- zRealtime.py
+++ zRealtime.py
@@ -1,8 +1,3 @@
-"""
-Mind Monitor - EEG OSC Receiver
-Coded: James Clutterbuck (2022)
-Requires: pip install python-osc
-"""
 from datetime import datetime
 from multiprocessing.connection import Client
 import time
@@ -10,16 +5,14 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 from codrone_edu.drone import *
 
-
 c = Client(("localhost", 5001))
 
-
+#Setup
 ip = "10.122.93.209"
 port = 5004
 filePath = 'test.csv'
 auxCount = -1
 recording = False
-
 theta = -1
 alpha = -1
 beta = -1
@@ -38,6 +31,16 @@ def writeFileHeader():
     fileString +='Marker\n'
     f.write(fileString)
 
+#Array for Theta, Beta, Alpha waves
+brainWaves = [0, 0, 0]
+
+#Engagement for divide-by-zero error
+lastEngagement = 0
+
+#Array for holding 5 values
+fiveValues = [0, 0, 0, 0, 0]
+
+#Send EEG recordings to server
 def eeg_handler(address: str,*args):
     global recording
     global auxCount
@@ -48,31 +51,50 @@ def eeg_handler(address: str,*args):
     global inAir
     global blinkCounter
     global old_time
+    global brainWaves
+    global lastEngagement
+    global fiveValues
     
     if auxCount==-1:
         auxCount = len(args)-4
         writeFileHeader()
+
+    #Each time we get 
     if recording:
         timestampStr = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         fileString = timestampStr
+
+        #Assign Theta, Beta, and Alpha wave values to brainWaves array
         if address == "/muse/elements/theta_absolute":
             theta = args[0]
-            # print("Theta: ", theta)
+            brainWaves[0] = theta
         elif address == "/muse/elements/beta_absolute":
             beta = args[0]
-            # print("Beta: ", beta)
+            brainWaves[1] = beta
         elif address == "/muse/elements/alpha_absolute":
             alpha = args[0]
-            # print("Alpha: ", alpha)
-        elif address == "/muse/elements/jaw_clench":
-            print("Jaw Clenched")
-            c.send("Jaw Clenched")
-            blinkCounter = 0
-        elif address == "/muse/elements/blink":
-            blinkCounter = blinkCounter + 1
-            print("Blinked: ", blinkCounter)
-            c.send("Blinked")
-    
+            brainWaves[2] = alpha
+        if brainWaves[0] != 0 and brainWaves[1] != 0 and brainWaves[2] != 0:
+
+            #Calculate engagement
+            if brainWaves[0] + brainWaves[2] != 0:
+                engagement = (abs(brainWaves[1]) / (abs(brainWaves[0]) + abs(brainWaves[2])))
+
+                #Calculate the average of 5 values
+                fiveValues.pop(0)
+                fiveValues.append(engagement)
+                average = abs(sum(fiveValues) / 5)
+
+                #Return engagement
+                tempString = timestampStr + " " + str(average)
+                print(tempString)
+                c.send(average)
+            else:
+                c.send(0)
+
+            #Reset brainWaves
+            brainWaves = [0, 0, 0]
+
 def marker_handler(address: str,i):
     global recording
     global auxCount
@@ -96,12 +118,10 @@ def marker_handler(address: str,i):
 if __name__ == "__main__":
     try:
         dispatcher = dispatcher.Dispatcher()
+
         dispatcher.map("/muse/elements/theta_absolute", eeg_handler)
         dispatcher.map("/muse/elements/alpha_absolute", eeg_handler)
         dispatcher.map("/muse/elements/beta_absolute", eeg_handler)
-        dispatcher.map("/muse/elements/jaw_clench", eeg_handler)
-        dispatcher.map("/muse/elements/blink", eeg_handler)
-
 
         dispatcher.map("/Marker/*", marker_handler)
 
@@ -110,7 +130,6 @@ if __name__ == "__main__":
         old_time = time.time()
         server.serve_forever()
         
-            
     except KeyboardInterrupt:
-        drone.close()
+        pass
     # drone.close()
